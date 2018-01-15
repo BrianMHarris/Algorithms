33b4bd5acb9e4a88c5b027f5988bb89713a98f6a
Brian Harris <brianharrisdesign@gmail.com>
2018-01-15 10:26:32 -0800

    fix to binary heap and extract function added

--- Data-Structures/BinaryTrees/BinaryHeap.js
+++ Data-Structures/BinaryTrees/BinaryHeap.js
@@ -6,6 +6,7 @@ class BinaryHeap {
 
   // compare the parent and the child based on type of heap
   compare(parent, child) {
+    if (parent === null || child === null) { return false; }
     if (this.type === "min") {
       return this.storage[parent] > this.storage[child];
     }
@@ -18,9 +19,8 @@ class BinaryHeap {
     let idx = this.storage.push(value) - 1;
 
     let bubble = (index) => {
-      let parent = Math.floor((idx - 1) / 2);
+      let parent = Math.floor((index - 1) / 2);
       if (parent < 0) { return; }
-
       while(this.compare(parent, index)) {
         [this.storage[parent], this.storage[index]] =
           [this.storage[index], this.storage[parent]];
@@ -33,8 +33,44 @@ class BinaryHeap {
     bubble(idx);
   }
 
+  // extracts the minimum or maximum value
+  //  can only remove the last node, so must do a special swapping
   extract() {
+    // first swap the first with the last value
+    if (this.storage.length > 1) {
+      [this.storage[0], this.storage[this.storage.length - 1]] =
+        [this.storage[this.storage.length - 1], this.storage[0]]
+    }
 
+    // pull the last value out, since it was formerly the first
+    let extracted = this.storage.pop();
+
+    // the heap condition is violated (out of order)
+    // so bubble down until it is not.
+    let bubbleDown = (index) => {
+      let left = index * 2 + 1;
+      let right = index * 2 + 2;
+      let swap = left;
+      if (this.storage[right] && this.storage[right] < this.storage[left]) {
+        swap = right;
+      }
+
+      // swap with the lowest values
+      while (this.compare(index, swap)) {
+        [this.storage[index], this.storage[swap]] =
+          [this.storage[swap], this.storage[index]];
+
+        left = index * 2 + 1;
+        right = index * 2 + 2;
+        swap = left;
+        if (this.storage[right] && this.storage[right] < this.storage[left]) {
+          swap = right;
+        }
+      }
+    }
+
+    bubbleDown(0);
+    return extracted;
   }
 }
 
@@ -46,5 +82,8 @@ function main() {
   heap.insert(3);
   heap.insert(1);
   heap.insert(2);
+  heap.insert(0);
+  console.log(heap.storage)
+  console.log("Extracted: " + heap.extract());
   console.log(heap.storage)
 }
